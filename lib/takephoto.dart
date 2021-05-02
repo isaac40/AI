6be4856ff2227d74ca27a2a467ce00357aaadc6f
isaac40 <0923113852@gmail.com>
2021-05-03 00:20:37 +0800

    takephoto 1

--- lib/takephoto.dart
+++ lib/takephoto.dart
@@ -2,14 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ImagePickerWidget extends StatefulWidget{
+class takephoto extends StatefulWidget{
   @override
   State<StatefulWidget> createState(){
     return _ImagePickerState();
   }
 }
 
-class _ImagePickerState extends State<ImagePickerWidget> {
+class _ImagePickerState extends State<takephoto> {
   var _imgPath;
 
   @override
@@ -18,12 +18,25 @@ class _ImagePickerState extends State<ImagePickerWidget> {
         appBar: AppBar(
           title: Text("拍照"),
         ),
-        body: SingleChildScrollView(
-          child: Column(
+        backgroundColor: Colors.cyan[400],
+        body: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: BorderSide(color: Colors.red)),
                 onPressed: _takePhoto,
-                child: Text("拍照"),
+                child: Text("拍照", style: TextStyle(fontSize: 60, color: Colors.black),),
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: BorderSide(color: Colors.red)),
+                onPressed: _openGallery,
+                child: Text("相簿", style: TextStyle(fontSize: 60, color: Colors.black),),
               ),
             ],
           ),
@@ -33,6 +46,13 @@ class _ImagePickerState extends State<ImagePickerWidget> {
   _takePhoto() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
 
+    setState(() {
+      _imgPath = image;
+    });
+  }
+
+  _openGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
       _imgPath = image;
     });
